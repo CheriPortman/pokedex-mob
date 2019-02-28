7f773c33e771bec2f58ec11ef2a229f765940a41
Chris Piccaro <chrispiccaro18@gmail.com>
2019-02-28 15:03:06 -0800

    refactored paging with updatePaging

--- src/paging-component.js
+++ src/paging-component.js
@@ -5,45 +5,32 @@ const nextButton = document.getElementById('next-button');
 
 const PER_PAGE = 30;
 
-let currentPageNumber = 1;
 
 export default function loadPaging(totalPokemans) {
     const totalPageCount = Math.ceil(totalPokemans / PER_PAGE);
     totalPages.textContent = totalPageCount;
-
-    previousButton.disabled = currentPageNumber === 1;
+    
+    let currentPageNumber = 1;
+    updatePaging();
 
     nextButton.addEventListener('click', () => {
         currentPageNumber++;
-        currentPage.textContent = currentPageNumber;
-
-        const pagingOptions = {
-            page: currentPageNumber,
-            perPage: PER_PAGE
-        };
-
-        console.log(pagingOptions);
-
-        nextButton.disabled = currentPageNumber === totalPageCount;
-        previousButton.disabled = currentPageNumber === 1;
-
-    
+        updatePaging();
     });
 
     previousButton.addEventListener('click', () => {
         currentPageNumber--;
-        currentPage.textContent = currentPageNumber;
+        updatePaging();
+    });
 
+    function updatePaging() {
+        currentPage.textContent = currentPageNumber;
         const pagingOptions = {
             page: currentPageNumber,
             perPage: PER_PAGE
         };
-
         console.log(pagingOptions);
-
         nextButton.disabled = currentPageNumber === totalPageCount;
         previousButton.disabled = currentPageNumber === 1;
-
-    
-    });
+    }
 } 
